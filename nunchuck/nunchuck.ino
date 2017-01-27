3f14622a0abb646afec38e8ad5324468810e4978
Neil Kelleher <neil.kelleher@gmail.com>
2017-01-27 15:57:50 -0800

    Refactor

--- nunchuck/nunchuck.ino
+++ nunchuck/nunchuck.ino
@@ -7,35 +7,42 @@
 
 #include <Wire.h>
 
-void setup()
+#define DEBUG 1
+#define SIMULATE 1
+
+int write2(byte a, byte b)
 {
-  Wire.begin();        // join i2c bus as master
-  
-  delay(250);
-  
-  Wire.beginTransmission(0x52);
-  Wire.write(0x40);
-  Wire.write(0x0);
-  //Wire.write(0x0);
-  //Wire.write(0x0);
+  Wire.beginTransmission(0x52);      // Nunchuck IIC address
+  Wire.write(a);
+  Wire.write(b);
   int r = Wire.endTransmission();
-  Serial.println(r);
-  
-  Wire.beginTransmission(0x52);
-  Wire.write(0xF0);
-  Wire.write(0x55);
-   r = Wire.endTransmission();
-  Serial.println(r);
-
-  Wire.beginTransmission(0x52);
-  Wire.write(0xFB);
-  Wire.write(0x00);
-   r = Wire.endTransmission();
+#ifdef DEBUG
+  Serial.print("IIC write return code: ");
   Serial.println(r);  
-
-  Mouse.begin();
+#endif
+  
+  return r;
 }
 
+void setup()
+{
+  Wire.begin();        // Join IIC bus as master
+  Serial.begin(9600);
+  
+  delay(250);        // let things settle
+  
+#ifdef DEBUG
+  delay(5000);        // Give time to open the serial monitor
+#endif
+
+  write2(0x40,0x00);  // OEM initialization
+  write2(0xF0,0x55);  // Alternate initialization, seems to work on clones
+  write2(0xFB,0x00);
+
+  Mouse.begin();    // start the mouse HID
+}
+
+// Give the mouse some acceleration the further it is pushed
 int map(int val)
 {
   if (val >4) val+=32;
@@ -44,66 +51,63 @@ int map(int val)
   if (val>96) val+=64;
   if (val<-96) val-=64;
   
-  return val /32;
-  
+  return val /32;        // scale back down
 }
 
 void loop()
 {
-
-  byte lastbut=3;
+  // Get next report
+  Wire.beginTransmission(0x52);
+  Wire.write(0x0);
+  Wire.endTransmission();
   
-  Wire.requestFrom(0x52, 6);    // request 6 bytes from slave device #2
+  Wire.requestFrom(0x52, 6);    // Request 6 bytes from nunchuck
 
-  if (6 == Wire.available())    // slave may send less than requested
+  if (6 == Wire.available())    // ... may send less than requested
   { 
+    // OEM device may need data 'decoding'
+    // i.e. c = (c ^ 0x17) + 0x17;
     int x = Wire.read();
-    x=x - 128;
-    
     int y = Wire.read();
-    y=y - 131;
     
+    // Gyro data - could be used for airmouse
     byte gx = Wire.read();
     byte gy = Wire.read();
     byte gz = Wire.read();
     
+    // Note buttons include the Gyro LSBs
     byte but = Wire.read();
-    but = but & 3;
-        
-    Mouse.move(map(x), -map(y), 0);
-    
-    //c = (c ^ 0x17) + 0x17;
-    #if 0
-    Serial.print(x);  
+
+#ifdef DEBUG
+    Serial.print("Raw X,Y: ");
+    Serial.print(x);
     Serial.print(" ");
     Serial.print(y);
-    Serial.print(" ");
+    
+    Serial.print(" Buttons: ");
     Serial.println(but);
-    #endif
-  
-    #if 0
-    if (but != lastbut)
-    {
-      if ((lastbut &1) && !(but &1)) Mouse.press(MOUSE_LEFT);
-      if (!(lastbut &1) && (but &1)) Mouse.release(MOUSE_LEFT);
-      if ((lastbut &2) && !(but &2)) Mouse.press(MOUSE_RIGHT);
-      if (!(lastbut &2) && (but &2)) Mouse.release(MOUSE_RIGHT);
-      
-      lastbut=but; 
-    }
-    #endif
+#endif
+
+    // Adjust these for centerpoint
+    x=x - 128;
+    y=y - 131;   
+    but = but & 3;        // Mask out the Gyro LSBs
+    
+    // When SIMULATE defined, don't actually move the mouse... 
+    // Good idea to ensure data is good first
+#ifndef SIMULATE
+    Mouse.move(map(x), -map(y), 0);
     
     if (but & 1) Mouse.release(MOUSE_LEFT); else Mouse.press(MOUSE_LEFT);
     if (but & 2) Mouse.release(MOUSE_RIGHT); else Mouse.press(MOUSE_RIGHT);
+#endif
 
   }
-  Wire.beginTransmission(0x52);
-  Wire.write(0x0);
-  //Wire.write(0x0);
-  //Wire.write(0x0);
-  int r = Wire.endTransmission();
-  
-  //Serial.println();
-  
+    
+#ifdef DEBUG
+  // Slow things down so we can see what's happening
+  delay(500);
+#endif
+
   delay(10);
 }
